a80b3d11f689836a0a2dc50dee33bae89d107c56
Carlos Fernandes <69088161+carlosfernandescrypt@users.noreply.github.com>
2024-06-16 17:04:18 -0300

    funções empilhar, desempilhar, topo e imprimirHastes

--- main.c
+++ main.c
@@ -26,3 +26,40 @@ int pilhaVazia(Pilha *p) {
 int pilhaCheia(Pilha *p) {
     return p->topo == MAX_DISCOS - 1;
 }
+// Função para empilhar um disco
+void empilhar(Pilha *p, int disco) {
+    if (!pilhaCheia(p)) {
+        p->discos[++(p->topo)] = disco;
+    }
+}
+
+// Função para desempilhar um disco
+int desempilhar(Pilha *p) {
+    if (!pilhaVazia(p)) {
+        return p->discos[(p->topo)--];
+    }
+    return -1;
+}
+
+// Função para obter o disco no topo da pilha sem desempilhar
+int topo(Pilha *p) {
+    if (!pilhaVazia(p)) {
+        return p->discos[p->topo];
+    }
+    return -1;
+}
+
+
+// Função para imprimir as hastes e os discos
+void imprimirHastes(Pilha hastes[3], int numDiscos) {
+    for (int i = numDiscos - 1; i >= 0; i--) {
+        for (int j = 0; j < 3; j++) {
+            if (i > hastes[j].topo) {
+                printf("    |    ");
+            } else {
+                printf("%4d    ", hastes[j].discos[i]);
+            }
+        }
+        printf("\n");
+    }
+}
